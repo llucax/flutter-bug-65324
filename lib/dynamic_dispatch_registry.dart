315d716ccb081057ad29dc744460b91208dea6b7
Leandro Lucarella <luca@llucax.com>
2020-09-06 23:47:37 +0200

    More reduction

--- lib/dynamic_dispatch_registry.dart
+++ lib/dynamic_dispatch_registry.dart
@@ -1,7 +1,10 @@
-// Here is where this gets REALLY WEIRD: Removing the last character of the
-// class comment fixes the issue. It seems to be tied to the length of the
+// Here is where this gets REALLY WEIRD: Removing the last character ("2") of
+// the class comment fixes the issue. It seems to be tied to the length of the
 // comment, as I replaced the last character with some others and it still
 // breaks the compiler. Same adding more characters to the comment.
+//
+// I used "2" as the last character just for consistency, to know if you remove
+// that one last, the compiler error goes away, but the "2" is not special.
 
 /// asdasdadasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasda.
 ///
@@ -14,19 +17,17 @@ class DynamicDispatchRegistry<B, T extends Function> {
 
   // removing this getter will also fix the issue, no matter how I enlarge or
   // change the "problematic comments"
+  // Removing the function or the comment altogether also fixes the issue.
   bool get isEmpty => _registry.isEmpty;
 
   // Same here, removing the last 2, fixes the issue, also seems length-related.
+  // Removing the function or the comment altogether also fixes the issue.
 
   /// xxaasdadadadasdadadadadasdasa2
   T register(Type type, T function) {
     final old = _registry[type];
     _registry[type] = function;
     return old;
-  }
-
-  T unregister(Type type) {
-    return _registry.remove(type);
   }
 
   // This comment is much more subtle, I could only replace real words with
